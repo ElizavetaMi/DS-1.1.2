3dea13354c9a88faf4747c67b86819939e1ca256
Liza <elizavetamihel9@gmail.com>
2025-04-10 18:03:58 +0300

    Тест падает

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,26 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSentData() {
+
+        String testData = "some data";
+
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body(testData)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("dataIncorrect", equalTo("some data"));
+
+    }
+}
